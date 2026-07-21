91de2d7f7a499d0a3c3b60afcc580d0c00c999e8
Anil Lama <131907522+virtualanil@users.noreply.github.com>
2026-07-21 20:21:06 +0545

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,8 @@
-settings.js
+// js/settings.js
+const GAME_SETTINGS = {
+    easy: { lives: 5, multiplier: 0.7 },
+    hard: { lives: 3, multiplier: 1.0 },
+    expert: { lives: 1, multiplier: 1.5 }
+};
+
+export default GAME_SETTINGS;
