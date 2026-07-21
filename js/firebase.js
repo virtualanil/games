60f52948e49b9437fc42ee93bd8ee4b64d6900da
Anil Lama <131907522+virtualanil@users.noreply.github.com>
2026-07-21 20:03:21 +0545

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNVdBXRTJMkACc46EUmn38Vy4Flz3vDOQ",
+    authDomain: "typing-game-c5e84.firebaseapp.com",
+    projectId: "typing-game-c5e84",
+    storageBucket: "typing-game-c5e84.firebasestorage.app",
+    messagingSenderId: "1017813191155",
+    appId: "1:1017813191155:web:af354c72711c6488562a86"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
